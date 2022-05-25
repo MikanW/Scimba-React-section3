035c306525de8f0fc65c22196cd7149c1a0b6ba8
mikan <zuxian.wan@gmail.com>
2022-05-25 22:46:28 +0800

    add meme data

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,23 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+    const handleOnClick = function() {
+        const meme = memesData.data.memes[Math.floor(Math.random()*memesData.data.memes.length)];
+        console.log(meme.url);
+    };
+    
     return(
         <div className="meme">
                 <div className="meme--inputs">
                     <input type="textInput" id="input1" className="meme--input" defaultValue="Shut up"></input>
                     <input type="textInput" className="meme--input" defaultValue="and take my money"></input>
                 </div>
-                <input type="button" value="Get a new meme image  🖼" className="meme--btn"></input>
+                <button
+                    className="meme--btn"
+                    onClick={handleOnClick}>
+                    Get a new meme image  🖼
+                </button>
         </div>
     )
 }
